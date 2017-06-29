124a71cb8ddda8f403eef5bcc7d92bd51d0e1bf2
Unknown <f.zwiers@am-impact.nl>
2017-06-29 09:42:28 +0200

    Don't send errors about error code 0

--- services/AmTools_ErrorsService.php
+++ services/AmTools_ErrorsService.php
@@ -3,45 +3,47 @@ namespace Craft;
 
 class AmTools_ErrorsService extends BaseApplicationComponent
 {
-	public function initErrorHandler()
-	{
-		\Yii::app()->onException = function($exceptionEvent)
-		{
-			$msg = array(
-				'code' => $exceptionEvent->exception->getCode(),
-				'message' => $exceptionEvent->exception->getMessage(),
-				'location' => $exceptionEvent->exception->getFile() . ' on line ' . $exceptionEvent->exception->getLine()
-			);
+    public function initErrorHandler()
+    {
+        \Yii::app()->onException = function($exceptionEvent)
+        {
+            $msg = array(
+                'code' => $exceptionEvent->exception->getCode(),
+                'message' => $exceptionEvent->exception->getMessage(),
+                'location' => $exceptionEvent->exception->getFile() . ' on line ' . $exceptionEvent->exception->getLine()
+            );
 
-			if ($msg['message'] != '')
-			{
-				AmTools_ErrorsService::sendErrorMail($msg);
-			}
-		};
-		\Yii::app()->onError = function($errorEvent)
-		{
-			$msg = array(
-				'code' => $errorEvent->code,
-				'message' => $errorEvent->message,
-				'file' => $errorEvent->file,
-				'line' => $errorEvent->line
-			);
+            if ($msg['message'] != '')
+            {
+                AmTools_ErrorsService::sendErrorMail($msg);
+            }
+        };
+        \Yii::app()->onError = function($errorEvent)
+        {
+            $msg = array(
+                'code' => $errorEvent->code,
+                'message' => $errorEvent->message,
+                'file' => $errorEvent->file,
+                'line' => $errorEvent->line
+            );
 
-			AmTools_ErrorsService::sendErrorMail($msg);
-		};
-	}
+            AmTools_ErrorsService::sendErrorMail($msg);
+        };
+    }
 
-	public function sendErrorMail($error)
-	{
-		$error = array_merge($error, array(
-			'server' => $_SERVER ?? '',
-			'session' => $_SESSION ?? '',
-			'request' => $_REQUEST ?? ''
-		));
-		mail(craft()->config->get('testToEmailAddress') != '' ? craft()->config->get('testToEmailAddress') : 'onderhoud@am-impact.nl', "Fout in Craft opgetreden op " . ($_SERVER["SERVER_NAME"] ?? 'onbekend'), print_r($error, 1));
-	}
+    public function sendErrorMail($error)
+    {
+        if (!empty($error['code']) && $error['code'] > 0) {
+            $error = array_merge($error, array(
+                'server' => !empty($_SERVER) ? $_SERVER : '',
+                'session' => !empty($_SESSION) ? $_SESSION : '',
+                'request' => !empty($_REQUEST) ? $_REQUEST : ''
+            ));
+            mail(craft()->config->get('testToEmailAddress') != '' ? craft()->config->get('testToEmailAddress') : 'onderhoud@am-impact.nl', "Fout in Craft opgetreden op " . (!empty($_SERVER["SERVER_NAME"]) ? $_SERVER["SERVER_NAME"] : 'onbekend'), print_r($error, 1));
+        }
+    }
 
-	public function send404Header()
+    public function send404Header()
     {
         header("HTTP/1.0 404 Not Found");
     }
